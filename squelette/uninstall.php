7c5b63335f4280a10ce53587ef0fa48fb0e563c8
Christophe HENRY <christophe.henry@sbgodin.fr>
2014-07-07 18:46:37 +0200

    Correction typo et unicode

--- squelette/uninstall.php
+++ squelette/uninstall.php
@@ -1,8 +1,8 @@
 <?php
 /* 
-	Le code contenu dans cette page ne sera �xecut� qu'� la d�sactivation du plugin 
-	Vous pouvez donc l'utiliser pour supprimer des tables MySQL, des dossiers, ou executer une action
-	qui ne doit se lancer qu'� la d�sinstallation ex :
+	Le code contenu dans cette page ne sera exécuté qu'à la désactivation du plugin 
+	Vous pouvez donc l'utiliser pour supprimer des tables MySQL, des dossiers, ou exécuter une action
+	qui ne doit se lancer qu'à la désinstallation ex :
 	
 	mysql_query('DROP TABLE `'.MYSQL_PREFIX.'plugin_squelette`');
 
